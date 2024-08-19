cdc94578387de36c57c02093dd167fbd0d79017a
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-08-19 17:41:05 +0700

    create a header

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,18 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
 };
