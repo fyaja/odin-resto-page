1a356181d8caf95d23a24fc29443c56348bfea37
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-08-19 21:36:25 +0700

    make 'navigation' works and add style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // index.js
 import "./styles.css";
+import { renderHomeElement } from "./home.js";
+import { renderMenuElement } from "./menu.js";
+import { renderContactElement } from "./contact.js";
+renderHomeElement()
+
+document.querySelector('.home').addEventListener('click', renderHomeElement)
+document.querySelector('.menu').addEventListener('click', renderMenuElement)
+document.querySelector('.contact').addEventListener('click', renderContactElement)
--- webpack.config.js
+++ webpack.config.js
@@ -24,19 +24,7 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-            },
-          },
-        ],
-      },
+      }
     ],
   },
 };
